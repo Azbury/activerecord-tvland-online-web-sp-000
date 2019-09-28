d5e55de2cccfb84eae6833720eb1116ff8772045
Austin Asbury <acasbury25@aol.com>
2019-09-28 03:23:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,13 +2,4 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
-
-  def build_network (network)
-
-  end
-
-  def network
-
-  end
-
 end
